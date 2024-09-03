3d601acfb90407e17ccf3d6abeeb3384f9408cca
Aditya Uphade <hexabytecode@gmail.com>
2024-09-03 16:57:53 +0530

    Added Counter Component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function Counter() {
+  return (
+    <div>
+      <h1>0</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Counter />);

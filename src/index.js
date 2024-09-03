22047acb2d0556d53fe1f3f61a11ffa6abb1d047
Aditya Uphade <hexabytecode@gmail.com>
2024-09-03 17:06:36 +0530

    Created componenent with useState

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import "./index.css";
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement(e) {
+    e.preventDefault();
+    setCount(count + 1);
+  }
+
+  function handleDecrement(e) {
+    e.preventDefault();
+    setCount(count - 1);
+  }
+
   return (
     <div>
-      <h1>0</h1>
-      <button>+</button>
-      <button>-</button>
+      <h1>{count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
     </div>
   );
 }
